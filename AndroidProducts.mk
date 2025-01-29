8af16c254b13c4dba5e9cb39fc0802f1a986ef2e
bntxperses <dbikramyt@gmail.com>
2025-01-29 18:58:22 +0530

    Alioth: Clover Bringups

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,12 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/clover_alioth.mk
+COMMON_LUNCH_CHOICES := \
+    clover_alioth-ap4a-user \
+    clover_alioth-ap4a-userdebug \
+    clover_alioth-ap4a-eng
--- clover_alioth.mk
+++ clover_alioth.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common clover stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
@@ -18,7 +18,11 @@ $(call inherit-product, device/xiaomi/alioth/device.mk)
 TARGET_USES_MIUI_CAMERA := true
 TARGET_INCLUDES_MIUI_CAMERA := true
 
-PRODUCT_NAME := lineage_alioth
+# Clover Kingdom
+CLOVER_BUILDTYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := clover_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
