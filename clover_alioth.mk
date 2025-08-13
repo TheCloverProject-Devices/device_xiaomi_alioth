70d71bdd1cf01745ca37c4663f43e76ca5606c23
bntxperses <dbikramyt@gmail.com>
2025-08-13 12:14:50 +0545

    Alioth: Clover Bringups

--- clover_alioth.mk
+++ clover_alioth.mk
@@ -8,17 +8,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common clover stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit Camera-related flags
 TARGET_USES_MIUI_CAMERA := true
 TARGET_INCLUDES_MIUI_CAMERA := true
 
+# Clover Kingdom
+CLOVER_BUILDTYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := clover_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
